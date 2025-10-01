98891c2aaae8267b9b7d97cb5097afb13a49809a
MUKE-coder <gmukejohnbaptist@gmail.com>
2025-10-01 21:23:58 +0300

    added image to readme

--- src/components/template/Footer.tsx
+++ src/components/template/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="font-tomorrow text-muted-foreground inline-flex justify-between text-[0.7rem] uppercase">
-      <p>Made by LuanRoger - Based in Brazil 🇧🇷</p>
+      <p>Made by JB - Desishub</p>
       <p>Powered by Electron</p>
     </footer>
   );
